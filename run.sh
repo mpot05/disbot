2a84d09dd9ab6dec4822945b51233e06b0c88cc5
sdxix <holymolyitsmelogurk@gmail.com>
2024-05-15 15:10:13 -0400

    fix autoupdate script

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 # runs the bot, routinely checking for updates from git.
 # a bit of a janky script sure, but its pretty convenient i guess.
 
-PYTHON_CMD="python3"
+PYTHON_CMD="python3.10"
 MAIN_PY="disbot.py"
 
 UPDATE_INTERVAL=30
@@ -11,8 +11,20 @@ UPDATE_INTERVAL=30
 REFRESH_CMD="git pull"
 
 PID=0
+
+function clean_up() {
+    if [$PID -ne 0]
+    then
+        kill $PID
+    fi
+
+    exit 0
+}
+
+trap clean_up SIGINT
+
 # while true
-while 1
+while [ 1 ]
 do
     # run the script
     $PYTHON_CMD $MAIN_PY &
@@ -24,7 +36,8 @@ do
     sleep $UPDATE_INTERVAL
 
     # kill process if it was succesfully instantiated
-    if [$PID -ne 0] then
+    if [$PID -ne 0]
+    then
         kill $PID
     fi
 
@@ -33,8 +46,4 @@ do
 
     # run the refresh command (in this instance, git pull)
     $REFRESH_CMD
-end
-
-if [$PID -ne 0] then
-    kill $PID
-fi
+done
